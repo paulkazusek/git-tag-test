75b7d2a169438724d01508395a57f37c51d772c9
paulkazusek <paul.kazusek@gmail.com>
2024-07-26 21:53:34 +0200

    Test point in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "point.hpp"
+
 int main()
 {
     std::cout << "Hello world" << std::endl;
+
+    constexpr Point point(0.0, 10.0);
+    std::cout << "p( " << point.x() << ", " << point.y() << " )" << std::endl;
 }
